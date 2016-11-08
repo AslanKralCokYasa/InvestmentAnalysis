8ee5c042ecaf2cbe1adfb18a2c4cdce94adf2e88
can <mehmetcansimsek@gmail.com>
2016-11-08 21:11:45 +0300

    Database

---
+++ Data/DataStructure/HistoricalDataBlock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DataStructure
+{
+    [Table("HistoricalDataBlock", Schema = "Application")]
+    public class HistoricalDataBlock
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Index("IX_SYMBOL", IsUnique = true)]
+        [Required]
+        public string Symbol { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime RecordDate { get; set; }
+
+        [Required]
+        public decimal LastPrice { get; set; }
+
+        [Required]
+        public decimal MinPrice { get; set; }
+
+        [Required]
+        public decimal MaxPrice { get; set; }
+
+        [Required]
+        public long Volume { get; set; }
+    }
+}
---
+++ Data/InvestmentAnalysisContext.cs
@@ -0,0 +1,42 @@
+﻿using Data.DataStructure;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Reflection;
+
+namespace Data
+{
+    public class InvestmentAnalysisContext : DbContext
+    {
+        public InvestmentAnalysisContext() : base("name=InvestmentAnalysisContext")
+        {
+            // After first deployment, uncomment next line for protect existing data..
+            //Database.SetInitializer<InvestmentAnalysisContext>(null);
+        }
+
+        public DbSet<FinancialInstrument> FinancialInstruments { get; set; }
+        public DbSet<HistoricalDataBlock> HistoricalDataBlocks { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(type => !String.IsNullOrEmpty(type.Namespace))
+                .Where(type => type.BaseType != null
+                        && type.BaseType.IsGenericType
+                        && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Configurations.Add(configurationInstance);
+            }
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
