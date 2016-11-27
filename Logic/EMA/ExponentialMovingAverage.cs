d3628cc94e5ad338913d328e7aa467a0d97f1966
Firat <firataltinoz@gmail.com>
2016-11-27 12:26:48 +0300

    EMA is added by FiratAltinoz.

---
+++ Logic/EMA/ExponentialMovingAverage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.ExponentialMovingAverage
+{
+    public class ExponentialMovingAverage
+    {
+        public double[] CalculateExponentialMovingAverage(double[] data, int window)
+        {
+            if (data.Length < window)
+                return null;
+
+            int diff = data.Length - window;
+            double[] newdata = data.Take(window).ToArray();
+            double factor = CalculateFactor(window);
+            double sma = Average(newdata);
+
+            IList<double> result = new List<double>();
+            result.Add(Math.Round(sma, 2));
+
+            for (int i = 0; i < diff; i++)
+            {
+                double prev = result[result.Count - 1];
+                double price = data[window + i];
+                double next = factor * (price - prev) + prev;
+                result.Add(Math.Round(next, 2));
+            }
+
+            return result.ToArray();
+        }
+
+        private double Sum(double[] data)
+        {
+            double sum = 0;
+
+            foreach (var d in data)
+            {
+                sum += d;
+            }
+
+            return sum;
+        }
+
+        private double Average(double[] data)
+        {
+            if (data.Length == 0)
+                return 0;
+
+            return Sum(data) / data.Length;
+        }
+
+        private double CalculateFactor(int days)
+        {
+            if (days < 0)
+                return 0;
+
+            return 2.0 / (days + 1);
+        }
+    }
+}
