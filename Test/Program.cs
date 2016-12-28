be2c4323e6fa2d28770ae2e8d0d92ce6925d1dfa
Firat <firataltinoz@gmail.com>
2016-12-28 19:03:56 +0300

    added

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,27 +12,21 @@ namespace Test
     {
         public static void Main(string[] args)
         {
+            GetProfitGainLossInfo();
+        }
+
+        public static ProfitGainLoss GetProfitGainLossInfo()
+        {
             using (InvestmentAnalysisContext context = new InvestmentAnalysisContext())
             {
                 context.Database.Initialize(false);
             }
 
-            //int lastYearData = 240;
-            //int firstLength = 21;
-            //int secondLength = 50;
-            //string stockName = "GARAN";
-
-            //Console.WriteLine("*********************************************");
-            //List<ResultInfo> resultInfoList = getDecisionResults(lastYearData, firstLength, secondLength, stockName);
-            //resultInfoList.ForEach(Print);
-            //Console.WriteLine("OK.");
-
-            //HelperMethods.CalculateRSI("GARAN", 20);
 
             HistoricalDataBlock[] historicalData = HelperMethods.GetHistoricalDataBlock("GARAN");
 
-            PeriodInfo maxProfitPeriodInfo = new PeriodInfo { ProfitOrLoss = 0.00 };
-            PeriodInfo maxLossPeriodInfo = new PeriodInfo { ProfitOrLoss = 0.00 };
+            PeriodInfo _maxProfitPeriodInfo = new PeriodInfo { ProfitOrLoss = 0.00 };
+            PeriodInfo _maxLossPeriodInfo = new PeriodInfo { ProfitOrLoss = 0.00 };
 
             double safe = 0.00;
 
@@ -42,117 +36,34 @@ namespace Test
                 {
                     if (fasterPeriod + 2 > slowerPeriod)
                         continue;
+                    TotalTransactionInfo _totalTransactionInfo = GetTotalTransactionInfo("GARAN", historicalData, fasterPeriod, slowerPeriod);
+                    safe = _totalTransactionInfo.safe;
 
-                    safe = EMA("GARAN", historicalData, fasterPeriod, slowerPeriod);
-
-                    if (safe > maxProfitPeriodInfo.ProfitOrLoss)
+                    if (safe > _maxProfitPeriodInfo.ProfitOrLoss)
                     {
-                        maxProfitPeriodInfo.FasterPeriod = fasterPeriod;
-                        maxProfitPeriodInfo.SlowerPeriod = slowerPeriod;
-                        maxProfitPeriodInfo.ProfitOrLoss = safe;
+                        _maxProfitPeriodInfo.FasterPeriod = fasterPeriod;
+                        _maxProfitPeriodInfo.SlowerPeriod = slowerPeriod;
+                        _maxProfitPeriodInfo.ProfitOrLoss = safe;
+                        _maxProfitPeriodInfo.totalTransactionInfo = _totalTransactionInfo;
                     }
 
-                    if (safe < maxLossPeriodInfo.ProfitOrLoss)
+                    if (safe < _maxLossPeriodInfo.ProfitOrLoss)
                     {
-                        maxLossPeriodInfo.FasterPeriod = fasterPeriod;
-                        maxLossPeriodInfo.SlowerPeriod = slowerPeriod;
-                        maxLossPeriodInfo.ProfitOrLoss = safe;
+                        _maxLossPeriodInfo.FasterPeriod = fasterPeriod;
+                        _maxLossPeriodInfo.SlowerPeriod = slowerPeriod;
+                        _maxLossPeriodInfo.ProfitOrLoss = safe;
+                        _maxLossPeriodInfo.totalTransactionInfo = _totalTransactionInfo;
                     }
                 }
             }
 
-            Console.WriteLine(maxProfitPeriodInfo.ToString());
-            Console.WriteLine(maxLossPeriodInfo.ToString());
+            Console.WriteLine(_maxLossPeriodInfo.ToString());
+            Console.WriteLine(_maxLossPeriodInfo.ToString());
 
-            //EMA("GARAN", historicalData, 18, 50);
-            //EMA("GARAN", historicalData, 19, 50);
-            //EMA("GARAN", historicalData, 20, 50);
-            //EMA("GARAN", historicalData, 21, 50);
-            //EMA("GARAN", historicalData, 22, 50);
-            //EMA("GARAN", historicalData, 23, 50);
-            //EMA("GARAN", historicalData, 24, 50);
-            //EMA("GARAN", historicalData, 25, 50);
-
-            //Console.WriteLine("*********************************************");
+            return new ProfitGainLoss { maxProfitPeriodInfo = _maxLossPeriodInfo, maxLossPeriodInfo = _maxLossPeriodInfo };
         }
 
-        private static List<ResultInfo> getDecisionResults(int lastYearData, int firstLength, int secondLength, string stockName)
-        {
-            List<ResultInfo> resultInfoList = new List<ResultInfo>();
-            HistoricalDataBlock[] historicalDataBlock = HelperMethods.GetHistoricalDataBlock(stockName, lastYearData);
-            double[] emaFirst = HelperMethods.CalculateEMA(stockName, lastYearData, firstLength);
-            double[] emaSecond = HelperMethods.CalculateEMA(stockName, lastYearData, secondLength);
-
-            if (emaFirst != null && emaFirst.Length > 0 && emaSecond != null && emaSecond.Length > 0 && historicalDataBlock != null && historicalDataBlock.Length > 0)
-            {
-                double[] emaTrimmedArray = emaFirst.Skip(secondLength - firstLength).ToArray();
-
-                for (int i = 0; i < emaTrimmedArray.Length; i++)
-                {
-                    Console.WriteLine(emaTrimmedArray[i].ToString("F") + " # " + emaSecond[i].ToString("F") + " # " + ((emaTrimmedArray[i] > emaSecond[i]) ? "+" : "-"));
-
-
-                    int forwardIndex = i + 1;
-                    int decisionProcessIndex = i + 2;
-                    if (forwardIndex < emaTrimmedArray.Length && decisionProcessIndex < emaTrimmedArray.Length)
-                    {
-                        double currentEmaFirstItem = emaTrimmedArray[i];
-                        double currentEmaSecondItem = emaSecond[i];
-                        double forwardEmaFirstItem = emaTrimmedArray[forwardIndex];
-                        double forwardEmaSecondItem = emaSecond[forwardIndex];
-                        HistoricalDataBlock historicalDataBlockItem = historicalDataBlock[forwardIndex];
-
-                        if (currentEmaFirstItem - currentEmaSecondItem > 0)
-                        {
-                            if (!(forwardEmaFirstItem - forwardEmaSecondItem > 0))
-                            {
-                                resultInfoList.Add(mapResultInfoObject(decisionProcessIndex, Convert.ToDouble(historicalDataBlockItem.LastPrice), historicalDataBlockItem.RecordDate, false));
-                            }
-                        }
-                        else
-                        {
-                            if (forwardEmaFirstItem - forwardEmaSecondItem > 0)
-                            {
-                                resultInfoList.Add(mapResultInfoObject(decisionProcessIndex, Convert.ToDouble(historicalDataBlockItem.LastPrice), historicalDataBlockItem.RecordDate, true));
-                            }
-                        }
-
-                    }
-
-                }
-            }
-
-            return resultInfoList;
-        }
-
-        private static ResultInfo mapResultInfoObject(int arrayIndex, double lastPrice, DateTime recordDate, bool isBuying)
-        {
-            ResultInfo resultInfoObj = new ResultInfo();
-            resultInfoObj.setArrayIndex(arrayIndex);
-            resultInfoObj.setBuyingDecision(isBuying);
-            resultInfoObj.setDecisionText(getDecisionText(isBuying));
-            resultInfoObj.setLastPrice(lastPrice);
-            resultInfoObj.setRecordDate(recordDate);
-            return resultInfoObj;
-        }
-
-        private static string getDecisionText(bool isBuying)
-        {
-            if (isBuying) return "BUY";
-            else return "SELL";
-        }
-
-        private static void Print(ResultInfo s)
-        {
-            //Console.WriteLine("ArrayIndex=" + s.getArrayIndex());
-            //Console.WriteLine("BuyingDecision=" + s.getBuyingDecision());
-            Console.WriteLine("DecisionText=" + s.getDecisionText());
-            Console.WriteLine("LastPrice=" + s.getLastPrice());
-            Console.WriteLine("RecordDate=" + s.getRecordDate().ToString("yyyy-MM-dd"));
-            Console.WriteLine("*********************************************");
-        }
-
-        private static double EMA(string symbol, HistoricalDataBlock[] historicalDataBlocks, int fasterPeriod, int slowerPeriod, int window = 0)
+        private static TotalTransactionInfo GetTotalTransactionInfo(string symbol, HistoricalDataBlock[] historicalDataBlocks, int fasterPeriod, int slowerPeriod, int window = 0)
         {
             Console.WriteLine("*********************************************");
             Console.WriteLine("SYMBOL = {0}, WINDOW = {1}, FASTER_PERIOD = {2}, SLOWER_PERIOD = {3}",
@@ -166,7 +77,7 @@ namespace Test
             //Console.WriteLine("BLOCK_LENGTH = {0}, DATA_LENGTH = {1}, EMA_FASTER_LENGTH = {2}, EMA_SLOWER_LENGTH = {3}", 
             //    historicalDataBlocks.Length, data.Length, emaFaster.Length, emaSlower.Length);
 
-            List<PositionInfo> positionInfoList = new List<PositionInfo>(); 
+            List<PositionInfo> _positionInfoList = new List<PositionInfo>(); 
 
             string previousSign = string.Empty;
             string currentSign = string.Empty;
@@ -193,8 +104,8 @@ namespace Test
                     string type = longPosition ? "LONG" : "SHORT";
 
                     positionChangeInfo = "*****" + " # " + historicalData.LastPrice.ToString("F") + " # " + type;
-                    
-                    positionInfoList.Add(new PositionInfo() { Type = type, Date = historicalData.RecordDate, Price = (double)historicalData.LastPrice });
+
+                    _positionInfoList.Add(new PositionInfo() { Type = type, Date = historicalData.RecordDate, Price = (double)historicalData.LastPrice });
                 }
 
                 //Console.WriteLine(historicalData.RecordDate.ToString("yyyy-MM-dd") + " # "
@@ -205,37 +116,45 @@ namespace Test
             }
 
             //Console.WriteLine("*********************************************");
-            Console.WriteLine("TOTAL POSITION COUNT: " + positionInfoList.Count);
+            Console.WriteLine("TOTAL POSITION COUNT: " + _positionInfoList.Count);
 
-            double safe = 0.00;
+            double _safe = 0.00;
 
-            if (1 == positionInfoList.Count % 2)
+            if (1 == _positionInfoList.Count % 2)
             {
-                positionInfoList.RemoveAt(positionInfoList.Count - 1);
+                _positionInfoList.RemoveAt(_positionInfoList.Count - 1);
             }
 
-            if (positionInfoList.Count > 0)
+            if (_positionInfoList.Count > 0)
             {
                 double price = 0.00;
 
-                for (int i = 0; i < positionInfoList.Count; i++)
+                for (int i = 0; i < _positionInfoList.Count; i++)
                 {
                     //Console.WriteLine(positionInfoList[i].ToString());
 
-                    price = positionInfoList[i].Price;
+                    price = _positionInfoList[i].Price;
 
-                    if ("SHORT".Equals(positionInfoList[i].Type))
+                    if ("SHORT".Equals(_positionInfoList[i].Type))
                     {
                         price = (-1) * price;
                     }
 
-                    safe = safe + price;
+                    _safe = _safe + price;
                 }
 
-                Console.WriteLine("TOTAL POSITION PROFIT / LOSS: " + safe);
+                Console.WriteLine("TOTAL POSITION PROFIT / LOSS: " + _safe);
             }
 
-            return safe;
+            return new TotalTransactionInfo { safe = _safe, positionInfoList = _positionInfoList, ratio = (((_safe) / (_positionInfoList[0].Price * 100)).ToString("F")) };
+
+            //TotalTransactionInfo totalTransactionInfo = new TotalTransactionInfo();
+            //x.positionInfoList = _positionInfoList;
+            //x.safe = _safe;
+            //x.ratio = (((_safe) / (_positionInfoList[0].Price * 100)).ToString("F")) ;
+           
+
+            //return _safe;
 
             //Console.WriteLine("*********************************************");
         }
@@ -258,6 +177,7 @@ namespace Test
         public int FasterPeriod;
         public int SlowerPeriod;
         public double ProfitOrLoss;
+        public TotalTransactionInfo totalTransactionInfo;
 
         public override string ToString()
         {
@@ -267,5 +187,19 @@ namespace Test
                 + "ProfitOrLoss: " + this.ProfitOrLoss
                 + ".......................";
         }
+    }
+
+    public class TotalTransactionInfo
+    {
+        public List<PositionInfo> positionInfoList;
+        public double safe;
+        public string ratio;
+    }
+
+    public class ProfitGainLoss
+    {
+        public PeriodInfo maxProfitPeriodInfo;
+        public PeriodInfo maxLossPeriodInfo;
+
     }
 }
---
+++ WebPresentation/Portfolio.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Test;
+
+namespace WebPresentation.Transactions.PortfolioManagement
+{
+    public partial class Portfolio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack)
+            {
+                BindData();
+            }
+        }
+
+        private void BindData()
+        {
+            ProfitGainLoss profitGainLostObj = Program.GetProfitGainLossInfo();
+
+            List<PositionInfo> positionInfoList = profitGainLostObj.maxProfitPeriodInfo.totalTransactionInfo.positionInfoList;
+            //portfolioListGridView.DataSource = positionInfoList;
+
+            DataTable dt = new DataTable();
+            DataRow dr = null;
+            dt.Columns.Add(new DataColumn("RowNumber", typeof(string)));
+            dt.Columns.Add(new DataColumn("Tip", typeof(string)));
+            dt.Columns.Add(new DataColumn("Fiyat", typeof(string)));
+            dt.Columns.Add(new DataColumn("Tarih", typeof(string)));
+
+            for(int i=0; i<positionInfoList.Count; i++)
+            {
+                PositionInfo item = positionInfoList[i];
+                dr = dt.NewRow();
+                dr["RowNumber"] = 1;
+                dr["Tip"] = item.Type;
+                dr["Fiyat"] = item.Price;
+                dr["Tarih"] = item.Date;
+                dt.Rows.Add(dr);
+            }
+
+           
+
+            //Store the DataTable in ViewState
+            ViewState["CurrentTable"] = dt;
+
+            portfolioListGridView.DataSource = dt;
+            portfolioListGridView.DataBind();
+
+
+        }
+
+
+        protected void addButton_ServerClick(object sender, EventArgs e)
+        {
+            //ORGANIZATION organizationToAdd = new ORGANIZATION();
+            //organizationToAdd.OrganizationName = newOrganizationNameText.ValueWithNull();
+            //organizationToAdd.SignBoardName = newSignBoardNameText.ValueWithNull();
+            //organizationToAdd.DescriptiveName = newDescriptiveNameText.ValueWithNull();
+            //organizationToAdd.FoundationDate = ConvertDateTime(newFoundationDateText.ValueWithNull());
+            //organizationToAdd.UserID = CurrentUserID;
+            //orgHelper.AddOrganization(organizationToAdd);
+            //ClearInputFields(addNewRecordContent);
+            //BindData();
+
+        }
+
+        protected void portfolioListGridView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            //contentOrgList.Attributes.CssStyle.Remove("overflow");
+            //ORGANIZATION organizationToUpdate = orgHelper.GetSingleOrganization(Convert.ToInt64(e.Keys["ID"]));
+            //if (organizationToUpdate != null)
+            //{
+            //    organizationToUpdate.OrganizationName = e.NewValues["OrganizationName"].ToString();
+            //    organizationToUpdate.SignBoardName = e.NewValues["SignBoardName"].ToString();
+            //    organizationToUpdate.DescriptiveName = e.NewValues["DescriptiveName"].ToString();
+            //    organizationToUpdate.FoundationDate = ConvertDateTime(e.NewValues["FoundationDate"].ToString());
+
+            //    orgHelper.UpdateOrganization(organizationToUpdate);
+            //}
+
+            //portfolioListGridView.EditIndex = -1;
+            //BindData();
+        }
+
+        protected void portfolioListGridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            //contentOrgList.Attributes.CssStyle.Add("overflow", "scroll");
+            //portfolioListGridView.EditIndex = e.NewEditIndex;
+            //BindData();
+        }
+
+        protected void portfolioListGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //contentOrgList.Attributes.CssStyle.Remove("overflow");
+            //long organizationID = Convert.ToInt64(e.Keys["ID"]);
+            //orgHelper.DeleteOrganization(new ORGANIZATION { ID = organizationID });
+            //BindData();
+        }
+
+        protected void portfolioListGridView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            //contentOrgList.Attributes.CssStyle.Remove("overflow");
+            //portfolioListGridView.EditIndex = -1;
+            //BindData();
+        }
+    }
+}
